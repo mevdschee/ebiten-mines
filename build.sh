d45a848d6d8f177a00772bccc2552fefc7bc2c0b
Maurits van der Schee <maurits@vdschee.nl>
2021-05-21 09:40:04 +0200

    update

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mkdir -p build
+TARGET=`basename "$PWD"`
+echo building $TARGET...
+GOOS=js GOARCH=wasm go build -o build/$TARGET.wasm .
+cp $(go env GOROOT)/misc/wasm/wasm_exec.js build
+cat << EOF > build/index.html
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <script src="wasm_exec.js"></script>
+    <script>
+      const go = new Go()
+      WebAssembly.instantiateStreaming(
+        fetch('$TARGET.wasm'),
+        go.importObject
+      ).then(result => {
+        go.run(result.instance)
+      })
+    </script>
+  </head>
+  <body></body>
+</html>
+EOF
+xdg-open http://localhost:8000
+cd build && python3 -m http.server 
